86617c6a1e845ca77730b4dd239e0c4d81b8a955
Albert <dow.albert@gmail.com>
2024-09-26 09:18:59 +0100

    feat; add for nipun

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div style="height:600px; width:800px">
+    <l-map ref="map" id="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+      <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
+        name="OpenStreetMap"></l-tile-layer>
+    </l-map>
+  </div>
+
+</template>
+
+<script>
+
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+import * as mapsgl from '@aerisweather/mapsgl';
+//  controller.addWeatherLayer("radar");
+//});
+export default {
+  name: 'App',
+  components: {
+    LMap,
+    LTileLayer,
+  },
+  data() {
+    return {
+      zoom: 2,
+      map: null,
+    };
+  },
+  methods: {
+    setupAerisMapgl() {
+      const account = new mapsgl.Account(
+        "CLIENT_ID",
+        "SECRET"
+      );
+      const controller = new mapsgl.LeafletMapController(this.$refs.map, {
+        account,
+      });
+      console.log(controller)
+      //controller.on("load", () => {
+      //  controller.addWeatherLayer("radar");
+      //});
+    }
+  },
+  mounted() {
+    this.$refs.map
+    this.setupAerisMapgl()
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
